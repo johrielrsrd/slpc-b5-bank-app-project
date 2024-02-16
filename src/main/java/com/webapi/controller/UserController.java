52f6c0f4fdcf79ccc8499fdfd5eb33bc447648e7
johrielrsrd <johrielrsrd@gmail.com>
2024-02-16 21:21:09 +0800

    made some minor updates.

--- src/main/java/com/webapi/controller/UserController.java
+++ src/main/java/com/webapi/controller/UserController.java
@@ -8,10 +8,8 @@ import com.webapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -55,5 +53,4 @@ public class UserController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
-
 }
