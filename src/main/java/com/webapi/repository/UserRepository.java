d167ae0ff7a1a76c00e8b950217d375c11568c51
Jpcrob <148976235+Jpcrob@users.noreply.github.com>
2024-02-16 17:54:30 +0800

    new

--- src/main/java/com/webapi/repository/UserRepository.java
+++ src/main/java/com/webapi/repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.webapi.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.webapi.entity.User;
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    List<User> findByUsernameAndPassword(String username, String password);
 
 }
